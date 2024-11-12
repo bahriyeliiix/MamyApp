a19d9fd8dba420f98247ac2c3cc890b8b9246260
DELL <DELL@DESKTOP-4RGAC49>
2024-11-12 14:26:55 +0300

    Mapping

--- MamyApp.Application/Mapping/MappingProfile.cs
+++ MamyApp.Application/Mapping/MappingProfile.cs
@@ -1,13 +1,23 @@
 ﻿using AutoMapper;
 using MamyApp.Core.Entities;  
 using MamyApp.Application.DTOs;
-using MamyApp.Application.Dtos; 
+using MamyApp.Application.Dtos;
+using MamyApp.Application.Features.Users.Commands;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
         CreateMap<User, UserDto>();
-        CreateMap<CreateUserDto, User>();     
+        CreateMap<UserDto, User>();
+
+        CreateMap<CreateUserDto, User>();    
+        CreateMap<User, CreateUserDto>();    
+        
+        CreateMap<CreateUserCommand, CreateUserDto>();
+        CreateMap<CreateUserDto, CreateUserCommand>();
+
+        CreateMap<CreateUserCommand, User>();
+        CreateMap<User, CreateUserCommand>();
     }
 }
