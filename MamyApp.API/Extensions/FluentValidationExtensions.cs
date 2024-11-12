d0bb9b4abec62836e761d5a89a5975c1596420ac
DELL <DELL@DESKTOP-4RGAC49>
2024-11-12 16:58:21 +0300

    Add Fluent

---
+++ MamyApp.API/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MamyApp.API.Extensions
+{
+    public static class FluentValidationExtensions
+    {
+        public static IServiceCollection AddFluentValidationServices(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<CreateUserDtoValidator>();
+            return services;
+        }
+    }
+}
---
+++ MamyApp.Application/Validator/CreateUserDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MamyApp.Application.Dtos;
+using MamyApp.Application.DTOs;
+
+public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        // Username boş olamaz, ve 3 ile 50 karakter arasında olmalı.
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required.")
+            .Length(3, 50).WithMessage("Username must be between 3 and 50 characters.");
+
+        // PasswordHash boş olamaz.
+        RuleFor(x => x.PasswordHash)
+            .NotEmpty().WithMessage("Password is required.");
+
+        // Email boş olamaz ve geçerli bir e-posta formatında olmalı.
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+    }
+}
